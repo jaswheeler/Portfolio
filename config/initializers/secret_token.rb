32a72a8ab517af84acb06dc542e4f1e01dcf361d
jwheeler <jwheeler@Jasons-MacBook-Air.local>
2012-10-30 10:28:46 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioManager::Application.config.secret_token = '0f107ce170ada8b6f8716055a4585aa42f6ee61abc969b3d48e9a9ae427d1881fd96949863fe4bde570efd054e2b9f3f0fd38acbce6e32d0c89f2a9a6049f83c'
