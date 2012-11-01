98797fc8814042c972a5ed7358c46cf2d2f66abb
jwheeler <jwheeler@Jasons-MacBook-Air.local>
2012-11-01 05:49:15 -0600

    Changes to README

--- test/unit/holding_test.rb
+++ test/unit/holding_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class HoldingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "symbol is required" do
+    holding = Holding.create(price: 100, qty: 100)
+    assert_equal ["can't be blank"], holding.errors[:symbol]
+  end
+
+  test "price is required" do
+    holding = Holding.create(symbol: 'GOOG', qty: 100)
+    assert_equal ["can't be blank", "is not a number"], holding.errors[:price]
+  end
+
+  test "qty is required" do
+    holding = Holding.create(symbol: 'GOOG', price: 100)
+    assert_equal ["can't be blank", "is not a number"], holding.errors[:qty]
+  end
+
 end
