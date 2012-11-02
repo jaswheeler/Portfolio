7f0ad393f9ec3baf65661ec6aed944c6952537e1
jwheeler <jwheeler@Jasons-MacBook-Air.local>
2012-11-02 10:28:14 -0600

    Added validation for invalid symbol

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,4 +1,9 @@
 class Holding < ActiveRecord::Base
   validates :symbol, :price, :qty, presence: true
   validates :price, :qty, numericality: true
+  validate :must_be_valid_symbol
+
+  def must_be_valid_symbol
+    errors.add(:symbol, "unknown symbol") unless is_valid
+  end
 end
--- test/unit/holding_test.rb
+++ test/unit/holding_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class HoldingTest < ActiveSupport::TestCase
-  test "symbol is required" do
+  test "symbol is required and valid" do
     holding = Holding.create(price: 100, qty: 100)
-    assert_equal ["can't be blank"], holding.errors[:symbol]
+    assert_equal ["can't be blank", "unknown symbol"], holding.errors[:symbol]
   end
 
-  test "price is required" do
+  test "price is required and a number" do
     holding = Holding.create(symbol: 'GOOG', qty: 100)
     assert_equal ["can't be blank", "is not a number"], holding.errors[:price]
   end
 
-  test "qty is required" do
+  test "qty is required and a number" do
     holding = Holding.create(symbol: 'GOOG', price: 100)
     assert_equal ["can't be blank", "is not a number"], holding.errors[:qty]
   end
